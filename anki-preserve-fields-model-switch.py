21e51954ace4b7f7f87af6b2e90ceeeac7bc770e
Glutanimate <glutanimate@users.noreply.github.com>
2016-04-11 15:27:06 +0200

    Fix bug where add-on would cause adding cards to fail

--- anki-preserve-fields-model-switch.py
+++ anki-preserve-fields-model-switch.py
@@ -15,37 +15,46 @@ from anki.hooks import wrap
 from aqt.addcards import AddCards
 
 def newOnReset(self, _old, model=None, keep=False):
-  # return _old(self, model=None, keep=False)
-  note = self.setupNewNote(set=False)
-  modelName = note.model()['name']
-  oldNote = self.editor.note
-  if oldNote:
-    oldModelName = oldNote.model()['name']
-  if oldNote and oldModelName != modelName:
-    oldFieldList = oldNote.keys()
-    newFieldList = note.keys()
-    sharedFieldList = [val for val in newFieldList if val in oldFieldList]
-    for n, f in enumerate(note.model()['flds']):
-      fieldName = f['name']
-      try:
-        oldFieldName = oldNote.model()['flds'][n]['name']
-      except IndexError:
-        oldFieldName = None
-      if fieldName in oldFieldList:
-        note[fieldName] = oldNote[fieldName]
-      elif oldFieldName not in sharedFieldList:
-        try:
-          note.fields[n] = oldNote.fields[n]
-        except IndexError:
-          pass
-      else:
-        pass
-
+    # return _old(self, model=None, keep=False)
+    oldNote = self.editor.note
+    note = self.setupNewNote(set=False)
+    modelName = note.model()['name']
+    flds = note.model()['flds']
+    if oldNote:
+        oldModelName = oldNote.model()['name']
+        if oldModelName != modelName:
+            # model changed
+            oldFields = oldNote.keys()
+            newFields = note.keys()
+            for n, f in enumerate(note.model()['flds']):
+                fieldName = f['name']
+                try:
+                    oldFieldName = oldNote.model()['flds'][n]['name']
+                except IndexError:
+                    oldFieldName = None
+                # copy identical fields
+                if fieldName in oldFields:
+                    note[fieldName] = oldNote[fieldName]
+                # set non-identical fields by field index
+                elif oldFieldName and oldFieldName not in newFields:
+                    try:
+                        note.fields[n] = oldNote.fields[n]
+                    except IndexError:
+                        pass
+        else:
+            # model identical
+            if not keep:
+                self.removeTempNote(oldNote)
+            for n in range(len(note.fields)):
+                try:
+                    if not keep or flds[n]['sticky']:
+                        note.fields[n] = oldNote.fields[n]
+                    else:
+                        note.fields[n] = ""
+                except IndexError:
+                    break
     self.editor.currentField = 0
     self.editor.setNote(note, focus=True)
-    # return _old(self, model=None, keep=False)
-  else:
-    return _old(self, model=None, keep=False)
 
 
 AddCards.onReset = wrap(AddCards.onReset, newOnReset, "around")
