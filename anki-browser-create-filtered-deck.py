83bb08e6c8692df3661517baaf748a2eb974d0de
Glutanimate <glutanimate@users.noreply.github.com>
2016-04-16 11:13:12 +0200

    Add anki-browser-create-filtered-deck

---
+++ anki-browser-create-filtered-deck.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+Anki Add-on: Create Filtered Deck from Browser
+
+Creates filtered deck based on current search
+
+Copyright: (c) Glutanimate 2016
+License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+"""
+
+from PyQt4.QtCore import SIGNAL
+from PyQt4.QtGui import QKeySequence
+from anki.hooks import addHook
+
+def createFilteredDeck(self):
+    search = unicode(self.form.searchEdit.lineEdit().text())
+    self.mw.onCram(search)
+    
+def setupMenu(self):
+    menu = self.form.menuEdit
+    menu.addSeparator()
+    a = menu.addAction('Create Filtered Deck based on this Search')
+    a.setShortcut(QKeySequence("Ctrl+Shift+D"))
+    self.connect(a, SIGNAL("triggered()"), lambda b=self: createFilteredDeck(b))
+
+
+addHook("browser.setupMenus", setupMenu)
