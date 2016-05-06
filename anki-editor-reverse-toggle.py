3f35a977b10a1901f9dcec07b91bb569cd5e103e
Glutanimate <glutanimate@users.noreply.github.com>
2016-05-07 01:13:18 +0200

    Add hotkey for toggling frozen state when used with Frozen fields add-on

--- anki-editor-reverse-toggle.py
+++ anki-editor-reverse-toggle.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
 """
-Anki Add-on: self Reverse Toggle
+Anki Add-on: Editor Reverse Toggle
 
 Simple addon to quickly toggle the reverse card for a given note
 
-Hotkey: Alt+Shift+B
-
 (requires setting up the reverse field name below)
+
+Hotkeys: 
+
+Alt+Shift+B         toggle reverse field
+Ctrl+Alt+Shift+B    toggle reverse field and control its frozen state
+                    (meant to be used with the Frozen Fields add-on)
 
 Copyright: Glutanimate 2015 (https://github.com/Glutanimate)
 License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
@@ -19,12 +23,26 @@ from anki.hooks import addHook
 # set reverse field name here
 rev_field_name = "Bidirektional"
 
-def toggleReverseField(self):
+
+def toggleFrozenState(self, state):
+    model = self.note.model()
+    for n, f in enumerate(model['flds']):
+        fieldName = f['name']
+        if fieldName == rev_field_name:
+            fieldNr = n
+            break
+    model['flds'][n]['sticky'] = state
+
+def toggleReverseField(self, freezeToggle=False):
     if rev_field_name in self.note:
         if self.note[rev_field_name] == "y":
             self.note[rev_field_name] = ""
+            if freezeToggle:
+                toggleFrozenState(self, False)
         else:
             self.note[rev_field_name] = "y"
+            if freezeToggle:
+                toggleFrozenState(self, True)
         self.web.eval("""
             if (currentField) {
               saveField("key");
@@ -34,8 +52,11 @@ def toggleReverseField(self):
 
 
 def onSetupButtons(self):
-    t = QShortcut(QKeySequence(Qt.ALT + Qt.SHIFT + Qt.Key_B), self.parentWindow)
+    t = QShortcut(QKeySequence("Alt+Shift+B"), self.parentWindow)
     t.connect(t, SIGNAL("activated()"),
               lambda : toggleReverseField(self))
+    t = QShortcut(QKeySequence("Ctrl+Alt+Shift+B"), self.parentWindow)
+    t.connect(t, SIGNAL("activated()"),
+              lambda : toggleReverseField(self, freezeToggle=True))
 
 addHook("setupEditorButtons", onSetupButtons)
