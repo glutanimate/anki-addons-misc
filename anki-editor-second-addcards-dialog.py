40683e4b26360b27a5d7b58f50fedcf1d432e390
Glutanimate <glutanimate@users.noreply.github.com>
2016-06-14 22:40:49 +0200

    Add anki-editor-second-addcards-dialog

---
+++ anki-editor-second-addcards-dialog.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+"""
+Anki Add-on: Second Add Cards Dialog
+
+Hotkey that launches a second AddCards dialog from AddCards 
+window. Can also be used to toggle between the two dialog
+instances. 
+
+Hotkey: Ctrl+Shift+A
+
+TODO: support for an arbitrary number AddCards dialogs
+
+Copyright: (c) Glutanimate 2016
+"""
+
+#============USER CONFIGURATION START===============
+
+open_editor_hotkey = "Ctrl+Shift+A"
+
+#==============USER CONFIGURATION END===============
+
+from aqt import dialogs, addcards
+from aqt.addcards import AddCards
+from aqt.qt import *
+from anki.hooks import addHook, remHook, wrap
+from anki.sound import clearAudioQueue
+from aqt.utils import saveGeom, restoreGeom
+
+dialogs._dialogs["AddCards2"] = [addcards.AddCards, None]
+
+def myInit(self, mw):
+    curDialogs = dialogs._dialogs
+    self.dialogName = "AddCards"
+    # existing instance → open second instance
+    if curDialogs[self.dialogName][1] != None:
+        self.dialogName = "AddCards2"
+        self.setWindowTitle(_("Add") + " 2")
+        restoreGeom(self, self.dialogName)
+
+def myReject(self):
+    if not self.canClose():
+        return
+    remHook('reset', self.onReset)
+    clearAudioQueue()
+    self.removeTempNote(self.editor.note)
+    self.editor.setNote(None)
+    self.modelChooser.cleanup()
+    self.deckChooser.cleanup()
+    self.mw.maybeReset()
+    # save geometry of current dialog
+    saveGeom(self, self.dialogName)
+    # close dialog
+    dialogs.close(self.dialogName)
+    QDialog.reject(self)
+
+def switchAddWindow(self):
+    curDialogs = dialogs._dialogs
+    # toggle between windows
+    if curDialogs["AddCards"][1] == self.parentWindow:
+        dialogs.open("AddCards2", self.mw)
+    else:
+        dialogs.open("AddCards", self.mw)
+
+def onSetupButtons(self):
+    t = QShortcut(QKeySequence(open_editor_hotkey), self.parentWindow)
+    t.connect(t, SIGNAL("activated()"),
+              lambda a=self: switchAddWindow(a))
+
+# -----------------HOOKS-------------------- #
+
+AddCards.__init__ = wrap(AddCards.__init__, myInit, "after")
+AddCards.reject = myReject
+addHook("setupEditorButtons", onSetupButtons)
