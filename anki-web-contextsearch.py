2755e500d224e99eab8fe32c509b8ff6ce924524
Glutanimate <glutanimate@users.noreply.github.com>
2017-01-17 22:46:55 +0100

    Update header; Add Alt-hotkeys

--- anki-web-contextsearch.py
+++ anki-web-contextsearch.py
@@ -17,25 +17,15 @@ Copyright: (c) Glutanimate 2015-2017
 License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 """
 
-"""
-Simple addon to quickly look up words in Anki's Browser
-
-Copyright: Glutanimate 2015-2017 (https://github.com/Glutanimate)
-Based on 'OSX Dictionary Lookup' by Eddie Blundell <eblundell@gmail.com>:
-https://gist.github.com/eddie/ff3d820fb267ae26ca0e
-
-License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-"""
-
 ### USER CONFIGURATION START
 
 # list of tuples of search provider names and urls.
 # '%s' will be replaced with the search term
 SEARCH_PROVIDERS = [
-    ("Google", u"https://www.google.com/search?&q=%s"),
-    ("Google Images", u"https://www.google.de/search?&tbm=isch&q=%s"),
-    ("Wikipedia (en)", u"https://en.wikipedia.org/w/index.php?search=%s"),
-    ("Wikipedia (de)", u"https://de.wikipedia.org/w/index.php?search=%s")
+    ("&Google", u"https://www.google.com/search?&q=%s"),
+    ("Google &Images", u"https://www.google.de/search?&tbm=isch&q=%s"),
+    ("&Wikipedia (en)", u"https://en.wikipedia.org/w/index.php?search=%s"),
+    ("Wikipedia (&de)", u"https://de.wikipedia.org/w/index.php?search=%s")
 ]
 
 ### USER CONFIGURATION END
@@ -63,11 +53,11 @@ def add_lookup_action(view, menu):
     if not selected:
         return
     suffix = (selected[:20] + '..') if len(selected) > 20 else selected
-    label = u'Search for "%s" in the Browser' % suffix
+    label = u'Search for "%s" in the &Browser' % suffix
     action = menu.addAction(label)
     action.connect(action, SIGNAL('triggered()'),
         lambda t=selected: lookup_browser(t))
-    search_menu = menu.addMenu(u'Search for "%s" with...' % suffix)
+    search_menu = menu.addMenu(u'&Search for "%s" with...' % suffix)
     for idx, provider in enumerate(SEARCH_PROVIDERS):
         a = search_menu.addAction(provider[0])
         a.connect(a, SIGNAL('triggered()'), 
