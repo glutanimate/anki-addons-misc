f3eb27d6dbf1096a458a6a0cbb5076f6a46345f2
Glutanimate <glutanimate@users.noreply.github.com>
2017-04-16 11:24:44 +0200

    Add a configuration option to control whether or not to use existing tags

--- anki-editor-indentation-formatter.py
+++ anki-editor-indentation-formatter.py
@@ -12,15 +12,19 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
 # USER CONFIGURATION START
 
-# default html tag to apply when there is no
+# Default html tag to apply when there is no
 # pre-existing formatting (e.g. div, p, blockquote)
 HTML_TAG = "p"
 
-# hotkeys
+# Whether or not to indent existing tags instead of applying
+# the tag above
+USE_EXISTING_TAGS = False
+
+# Hotkeys
 HOTKEY_INDENT = "Alt+L"
 HOTKEY_OUTDENT = "Alt+J"
 
-# indentation steps in px (default: 40)
+# Indentation steps in px (default: 40)
 INDENTATION_STEP = 40
 
 # USER CONFIGURATION END
@@ -28,50 +32,61 @@ INDENTATION_STEP = 40
 from aqt.editor import Editor
 from anki.hooks import wrap
 
-def onIndent(self, mode):
-    self.web.eval("""
-        function indent(mode){
-            var elm = window.getSelection().focusNode;
-            var parent = window.getSelection().focusNode.parentNode;
-            var isElm = parent.isContentEditable && elm.toString() !== "[object Text]";
-            var isParElm = parent.isContentEditable && parent.parentNode.isContentEditable;
-            var newNode = false
 
-            if (mode == "in" && !isElm && !isParElm){
-                document.execCommand("formatBlock", false, "%(tag)s");
-                var elm = window.getSelection().focusNode;
-                if (elm.tagName !== "%(tag)s") {
-                    elm = elm.parentNode;
-                }
-                var marginL = %(step)i
-                var newNode = true
-            } else if (isElm || isParElm) {
-                if (!isElm) {
-                    elm = parent;
-                }
-                mleft = parseInt(elm.style.marginLeft)
-                if (isNaN(mleft)){
-                    mleft = 0;
-                }
-                if (mode == "in"){
-                    var marginL = mleft + %(step)i
-                } else {
-                    var marginL = Math.max(mleft-%(step)i, 0)
-                }
-            } else {
-                return
-            }
+js_indent = """
+function indent(mode){
+    var elm = window.getSelection().focusNode;
+    var parent = window.getSelection().focusNode.parentNode;
+    if (%(exst)s){
+        var isElm = parent.isContentEditable && elm.toString() !== "[object Text]";
+        var isParElm = parent.isContentEditable && parent.parentNode.isContentEditable;
+    } else {
+        var isElm = elm.tagName == "%(tag)s" 
+            && parent.isContentEditable;
+        var isParElm = parent.tagName == "%(tag)s"
+            && parent.parentNode.isContentEditable;
+    }
+    var newNode = false
 
-            if (newNode) {
-                elm.style.margin = "0px";
-            }
-            elm.style.marginLeft = marginL + "px";
-            
+    if (mode == "in" && !isElm && !isParElm){
+        document.execCommand("formatBlock", false, "%(tag)s");
+        var elm = window.getSelection().focusNode;
+        if (elm.tagName !== "%(tag)s") {
+            elm = elm.parentNode;
         }
-        indent("%(mode)s");
-        saveField('key');
-        """ % dict(tag=HTML_TAG.upper(), mode=mode, step=INDENTATION_STEP))
+        var marginL = %(step)i
+        var newNode = true
+    } else if (isElm || isParElm) {
+        if (!isElm) {
+            elm = parent;
+        }
+        mleft = parseInt(elm.style.marginLeft)
+        if (isNaN(mleft)){
+            mleft = 0;
+        }
+        if (mode == "in"){
+            var marginL = mleft + %(step)i
+        } else {
+            var marginL = Math.max(mleft-%(step)i, 0)
+        }
+    } else {
+        return
+    }
 
+    if (newNode) {
+        elm.style.margin = "0px";
+    }
+    elm.style.marginLeft = marginL + "px";
+    
+}
+indent("%%s");
+saveField('key');
+""" % dict(exst=str(USE_EXISTING_TAGS).lower(), tag=HTML_TAG.upper(), 
+            step=INDENTATION_STEP)
+
+
+def onIndent(self, mode):
+    self.web.eval(js_indent % mode)
 
 def setupButtons(self):
     self._addButton("OutdentBtn", lambda: self.onIndent("out"),
