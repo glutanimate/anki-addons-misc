20eea3dba26c75aca49c0271e18aa1bfc587772c
Glutanimate <glutanimate@users.noreply.github.com>
2017-06-21 13:43:09 +0200

    Adjustments for Image Occlusion and Note Organizer

--- anki-editor-field-navigation.py
+++ anki-editor-field-navigation.py
@@ -11,19 +11,26 @@ def changeFocusTo(self, fldnr):
     fldnr = fldnr - 1
     fldstot = len(self.note.fields) -1
     if fldnr >= fldstot or fldnr < 0:
-      fldnr = fldstot
+        # ignore onid field (Note Organizer add-on)
+        if self.note.model()['flds'][-1]['name'] == "onid":
+            fldnr = fldstot - 1
+        else:
+            fldnr = fldstot
+    elif self.note.model()['flds'][0]['name'] == "ID (hidden)":
+        # ignore hidden ID field (Image Occlusion Enhanced)
+        fldnr += 1
     self.web.setFocus()
     self.web.eval("focusField(%d);" % int(fldnr))
 
 def onSetupButtons(self):
     for i in range(0,10):
-      s = QShortcut(QKeySequence("Ctrl" + "+" + str(i)), self.parentWindow)
-      s.connect(s, SIGNAL("activated()"),
+        s = QShortcut(QKeySequence("Ctrl" + "+" + str(i)), self.parentWindow)
+        s.connect(s, SIGNAL("activated()"),
                 lambda f=i : changeFocusTo(self, f))
 
     s = QShortcut(QKeySequence("Alt+Shift+F"), self.parentWindow)
     s.connect(s, SIGNAL("activated()"),
-              lambda : self.web.setFocus())
+        lambda : self.web.setFocus())
 
 
 addHook("setupEditorButtons", onSetupButtons)
