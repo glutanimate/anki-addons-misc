57dbd4aadd1e34727d2e06da68f91a6250e5bbf3
Glutanimate <glutanimate@users.noreply.github.com>
2017-06-27 01:31:08 +0200

    Ability to modify TagEdit background color

--- anki-editor-custom-css.py
+++ anki-editor-custom-css.py
@@ -9,14 +9,27 @@ Copyright: (c) Glutanimate 2017
 License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 """
 
+######### USER CONFIGURATION START ##########
+
+TAGS_BACKGROUND = "#F5F6CE"
+
+######### USER CONFIGURATION END ##########
+
 import os
 
 from aqt import mw
 from aqt import editor
 
-from anki.hooks import addHook
+from anki.hooks import addHook, wrap
+
+def updateTagsBackground(self):
+    """Modify tagEdit background color"""
+    self.tags.setStyleSheet(
+        """QLineEdit {{ background: {}; }}""".format(
+            TAGS_BACKGROUND))
 
 def profileLoaded():
+    """Import modified CSS code into editor"""
     media_dir = mw.col.media.dir()
     css_path = os.path.join(media_dir, "_editor.css")
     if not os.path.isfile(css_path):
@@ -29,3 +42,6 @@ def profileLoaded():
     editor._html = editor._html + editor_style
 
 addHook("profileLoaded", profileLoaded)
+
+editor.Editor.setupTags = wrap(editor.Editor.setupTags, 
+                               updateTagsBackground, "after")
