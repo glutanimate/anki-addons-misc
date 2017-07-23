caa2084c37eb86b09ea0de2688445069d82e6585
Glutanimate <glutanimate@users.noreply.github.com>
2017-07-23 16:09:15 +0200

    Refactor add-on, make sorting column customizable

--- anki-browser-refresh.py
+++ anki-browser-refresh.py
@@ -1,32 +1,59 @@
 # -*- coding: utf-8 -*-
 
 """
-Anki Add-on: Refresh browser list
+Anki Add-on: Refresh Browser List
 
-Refreshes browser view and sets card sorting to "creation time"
+Refreshes browser view and optionally changes the sorting column
 (e.g. to show newly added cards since last search)
 
-Copyright: (c) Glutanimate 2016
+Copyright: (c) Glutanimate 2016-2017
 License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 """
- 
-from PyQt4.QtCore import SIGNAL
-from PyQt4.QtGui import QKeySequence
+
+# Do not modify the following line
+from __future__ import unicode_literals
+
+######## USER CONFIGURATION START ########
+
+SORTING_COLUMN = "noteCrt"
+# Custom column sorting applied on hotkey toggle
+#   - only works if that column is active in the first place
+#   - set to note creation time by default ("noteCrt")
+#   - can be disabled by setting SORTING_COLUM = ""
+#
+# Valid Values (regular browser):
+#
+# 'question' 'answer' 'template' 'deck' 'noteFld' 'noteCrt' 'noteMod'
+# 'cardMod' 'cardDue' 'cardIvl' 'cardEase' 'cardReps' 'cardLapses'
+# 'noteTags' 'note'
+#
+# Additional values (advanced browser):
+#
+# 'cfirst' 'clast' 'cavgtime' 'ctottime' 'ntags' 'coverdueivl' 'cprevivl'
+
+
+######## USER CONFIGURATION END ########
+
+from aqt.qt import *
+from aqt.browser import Browser
 from anki.hooks import addHook
 
-def refreshBrowserView(self):
+def refreshView(self):
     self.onSearch(reset=True)
-    if u'noteCrt' in self.model.activeCols:
-        col_index = self.model.activeCols.index(u'noteCrt')
-        self.onSortChanged(col_index, True)
-    self.form.tableView.selectRow(0)
+    if SORTING_COLUMN:
+        try:
+            col_index = self.model.activeCols.index(SORTING_COLUMN)
+            self.onSortChanged(col_index, True)
+            self.form.tableView.selectRow(0)
+        except ValueError:
+            pass
 
 def setupMenu(self):
     menu = self.form.menuEdit
     menu.addSeparator()
     a = menu.addAction('Refresh View')
     a.setShortcut(QKeySequence("F5"))
-    self.connect(a, SIGNAL("triggered()"), lambda b=self: refreshBrowserView(b))
+    a.triggered.connect(self.refreshView)
 
-
+Browser.refreshView = refreshView
 addHook("browser.setupMenus", setupMenu)
