385087de1242783abd65ff8917ffdfd17f1bd4fb
Glutanimate <glutanimate@users.noreply.github.com>
2017-07-25 23:58:53 +0200

    Refactored prefs, add-on structure; added custom icons

--- anki-browser-sticky-searches/anki-browser-sticky-searches.py
+++ anki-browser-sticky-searches/anki-browser-sticky-searches.py
@@ -34,14 +34,61 @@ from __future__ import unicode_literals
 
 ############## USER CONFIGURATION START ##############
 
-# HOTKEYS
+# CHECKBOXES
 
-# (set to "" to disable both the corresponding checkbox and hotkey)
-STICKY_TOGGLE_HOTKEY = "Alt+S" # Default: Alt+S
-DECK_TOGGLE_HOTKEY = "Alt+D" # Default: Alt+D
-TAGS_TOGGLE_HOTKEY = "Alt+T" # Default: Alt+T
-CARD_TOGGLE_HOTKEY = "Alt+C" # Default: Alt+C
-IS_TOGGLE_HOTKEY = "" # Deafult: "" (disabled)
+# You can set-up additional checkboxes here if you know what you are doing
+#
+# - checkboxes with an empty "value" entry ("value": None) set their sticky
+#   search tokens dynamically, i.e. by parsing the current query for the supplied
+#   prefix (e.g. "tag:") and saving all values of that particular prefix
+#
+# Example of an additional entry for a dynamic "is" checkbox:
+#
+# "is": {
+#     "hotkey": "Alt+I",
+#     "label": "I",
+#     "icons": "dot",
+#     "tooltip": "Limit results to current 'is' state", 
+#     "prefix": "is:",
+#     "value": None
+# },
+checkboxes = {
+    "sticky": {
+        "hotkey": "Alt+S", # Default: Alt+S
+        "icons": "snowflake",
+        "tooltip": "Sticky current search", 
+        "prefix": None,
+        "value": None
+    },
+    "deck": {
+        "hotkey": "Alt+D", # Default: Alt+D
+        "label": "D",
+        "icons": "dot",
+        "tooltip": "Limit results to current deck", 
+        "prefix": "deck:",
+        "value": "current"
+    },
+    "tags": {
+        "hotkey": "Alt+T", # Default: Alt+T
+        "label": "T",
+        "icons": "dot",
+        "tooltip": "Limit results to current tag selection", 
+        "prefix": "tag:",
+        "value": None
+    },
+    "card": {
+        "hotkey": "Alt+C", # Default: Alt+C
+        "label": "C",
+        "icons": "dot",
+        "tooltip": "Limit results to first card of each note", 
+        "prefix": "card:",
+        "value": "1"
+    },
+}
+
+# Any checkboxes you add will also have to be appended to the following tuple
+# Entries you remove from this list will be disabled
+order = ("sticky", "deck", "tags", "card", "deck1")
 
 # OPTIONS
 
@@ -62,56 +109,6 @@ from anki.hooks import wrap
 
 ##############  VARIABLES START  ##############
 
-# You can set-up additional checkboxes here if you know what you are doing
-#
-# - checkboxes with an empty "value" entry ("value": None) set their sticky
-#   search tokens dynamically, i.e. by parsing the current query for the supplied
-#   prefix (e.g. "tag:") and saving all values of that particular prefix
-# - please do not use any of the following as dictionary keys: tokens, last
-checkboxes = {
-    "sticky": {
-        "hotkey": STICKY_TOGGLE_HOTKEY,
-        "label": "S",
-        "tooltip": "Sticky current search", 
-        "default": Qt.Unchecked,
-        "prefix": None,
-        "value": None
-    },
-    "deck": {
-        "hotkey": DECK_TOGGLE_HOTKEY,
-        "label": "D",
-        "tooltip": "Limit results to current deck", 
-        "default": Qt.Unchecked,
-        "prefix": "deck:",
-        "value": "current"
-    },
-    "tags": {
-        "hotkey": TAGS_TOGGLE_HOTKEY,
-        "label": "T",
-        "tooltip": "Limit results to current tag selection", 
-        "default": Qt.Unchecked,
-        "prefix": "tag:",
-        "value": None
-    },
-    "card": {
-        "hotkey": CARD_TOGGLE_HOTKEY,
-        "label": "C",
-        "tooltip": "Limit results to first card of each note", 
-        "default": Qt.Unchecked,
-        "prefix": "card:",
-        "value": "1"
-    },
-    "is": {
-        "hotkey": IS_TOGGLE_HOTKEY,
-        "label": "I",
-        "tooltip": "Limit results to current 'is' state", 
-        "default": Qt.Unchecked,
-        "prefix": "is:",
-        "value": None
-    },
-}
-# Any checkboxes you add will also have to be appended to the following tuple:
-order = ("sticky", "deck", "tags", "card", "is")
 
 # Disable sticky tokens for the following searches:
 empty_queries = (
@@ -119,13 +116,58 @@ empty_queries = (
     "is:current"
 )
 
-default_prefs = {key: checkboxes[key]["default"] for key in order}
-# FIXME: more elegant implementation for 'tokens' and 'last' storage
-default_prefs["tokens"] = []
-default_prefs["stickied"] = ""
-default_prefs["last"] = {}
+# Default preferences dictionary
+default_prefs = {
+    "state": {},
+    "last": {},
+}
 
+# Checkbox styling
+icon_path = os.path.join(mw.pm.addonFolder(), "sticky_searches")
+
+iconsets = {
+    "snowflake": (
+        os.path.join(icon_path, "sticky.png"),
+        os.path.join(icon_path, "unsticky.png")
+    ),
+    "dot": (
+        os.path.join(icon_path, "active.png"),
+        os.path.join(icon_path, "inactive.png")
+    ),
+}
+
+cb_stylesheet = """
+QCheckBox::indicator {{
+    width: 18px;
+    height: 18px;
+}}
+QCheckBox::indicator:checked {{
+    image: url({0});
+}}
+QCheckBox::indicator:unchecked {{
+    image: url({1});
+}}
+QCheckBox::indicator:checked:hover {{
+    image: url({0});
+}}
+QCheckBox::indicator:unchecked:hover {{
+    image: url({1});
+}}
+QCheckBox::indicator:checked:pressed {{
+    image: url({0});
+}}
+QCheckBox::indicator:unchecked:pressed {{
+    image: url({1});
+}}
+QCheckBox::indicator:checked:disabled {{
+    image: url({0});
+}}
+QCheckBox::indicator:unchecked:disabled {{
+    image: url({0});
+}}
+"""
 ##############  VARIABLES END  ##############
+
 
 def tokenize(query):
     """Tokenize search query, adapted from anki.find.Finder"""
@@ -179,101 +221,114 @@ def uniqueList(seq):
     return [x for x in seq if not (x in seen or seen_add(x))]
 
 
+def sortTokens(s):
+    """Custom sort function to override token order"""
+    if s.startswith("deck:"):
+        return 1
+    elif s.startswith("tag:"):
+        return 2
+    elif s.startswith("is:"):
+        return 3
+    elif s.startswith("card:"):
+        return 4
+    else:
+        return s
+
 def onSearch(self, reset=True):
     """Intercept search and modify query with our tokens"""
-    sticky_query = ""
     query = unicode(self.form.searchEdit.lineEdit().text()).strip()
+    sticky = self.cbPrefs["last"].get("sticky", "")
+    cb_state = self.cbPrefs["state"]
+
     if query in empty_queries or (EMPTY_CLEAR and query == ""):
         return
-    if not any(self.cbState[i] for i in order):
+    if not any(i for i in cb_state.values()):
         # all checkboxes inactive
         return
-    if self.cbState["stickied"]:
-        sticky_query = self.cbState["stickied"]
-        query = query.replace(sticky_query, "")
+    
+    if sticky:
+        query = query.replace(sticky, "")
+    
     cur_tokens = tokenize(query)
-    new_tokens = uniqueList(self.cbState["tokens"] + cur_tokens)
-    new_query = " ".join([sticky_query] + new_tokens)
+    new_tokens = uniqueList(self.cbTokens + cur_tokens)
+    new_query = " ".join([sticky] + new_tokens)
+    
     self.form.searchEdit.lineEdit().setText(new_query)
-
-    # # filter out conflicting tokens
-    # for key, cb in checkboxes.items():
-    #     prefix = cb["prefix"]
-    #     if cb["value"]:
-    #         term = prefix + cb["value"]
-    #         if term in cur_tokens:
-    #             cur_tokens.remove(term)
-    #     if not self.cbState[key] or not cb.get("unique", False):
-    #         continue
-    #     dupes = cb.get("dupes", [])
-    #     cur_tokens = [t for t in cur_tokens
-    #                   if not t in dupes or t.startswith(prefix)]
 
 
 def onCbStateChanged(self, state, key):
     """Update persistent search tokens and search bar"""
 
+    if state == Qt.Checked:
+        mode = "add"
+    else:
+        mode = "remove"
+
     # Tokenize search and get existing sticky tokens
     query = self.form.searchEdit.lineEdit().text().strip()
-    cb_tokens = self.cbState["tokens"]
+    cb_tokens = self.cbTokens
+    sticky = self.cbPrefs["last"].get("sticky", "")
+    
     if query not in empty_queries or (EMPTY_CLEAR and query != ""):
         query_tokens = tokenize(query)
     else:
         query_tokens = []
     uniq_tokens = list(set(query_tokens) - set(cb_tokens))
     
-    if state == Qt.Checked:
-        mode = "add"
-    else:
-        mode = "remove"
-
-    sticky_query = ""
-    stickied_changed = False
-    
-    # Update sticky tokens
+    # Update tokens and sticky prefix
     prefix = checkboxes[key].get("prefix", None)
     value = checkboxes[key].get("value", None)
     if value:
+        # static checkbox
         token = prefix + value
         if mode == "add" and token not in cb_tokens:
             cb_tokens.append(token)
         elif mode == "remove" and token in cb_tokens:
             cb_tokens.remove(token)
     elif prefix:
+        # dynamic checkbox
         prefixes = (prefix, "-" + prefix)
         if mode == "add":
             to_add = [t for t in query_tokens if t.startswith(prefixes)]
             if to_add:
-                self.cbState["last"][key] = to_add
+                self.cbPrefs["last"][key] = to_add
             else:
-                to_add = self.cbState["last"].get(key, [])
-            cb_tokens = list(set(cb_tokens + to_add))
+                to_add = self.cbPrefs["last"].get(key, None)
+            if to_add:
+                cb_tokens = list(set(cb_tokens + to_add))
         elif mode == "remove":
             cb_tokens = [t for t in cb_tokens if not t.startswith(prefixes)]
-    else:
-        stickied_changed = True
+    elif key == "sticky":
+        # sticky search toggle
         if mode == "add":
-            sticky_query = query
+            sticky = query
+            for token in cb_tokens:
+                sticky.replace(token, "")
+            query = query.replace(sticky, "")
         else:
-            q = query.replace(self.cbState["stickied"], "")
-            self.form.searchEdit.lineEdit().setText(q)
-            sticky_query = ""
+            query = query.replace(sticky, "")
+            sticky = ""
+    else:
+        # should not happen
+        return False
 
-    # Update search bar
-    if query_tokens and not stickied_changed:
-        new_query_tokens = uniqueList(cb_tokens + uniq_tokens)
-        self.form.searchEdit.lineEdit().setText(" ".join(new_query_tokens))
+    # Save tokens or sticky prefix and update search bar
+    if key != "sticky": 
+        if query_tokens:
+            new_query_tokens = uniqueList(cb_tokens + uniq_tokens)
+            self.form.searchEdit.lineEdit().setText(" ".join(new_query_tokens))
+        self.cbTokens = sorted(cb_tokens, key=sortTokens)
+    else:
+        self.form.searchEdit.lineEdit().setText(sticky + " " + query)
+        self.cbPrefs["last"]["sticky"] = sticky
 
-    # Save state and reset
-    self.cbState[key] = state
-    self.cbState["tokens"] = cb_tokens
-    self.cbState["stickied"] = sticky_query
+    # Save checkbox state and reset
+    self.cbPrefs["state"][key] = state
     self.onReset()
 
     # DEBUG
-    print("{} is {}. New sticky tokens: ".format(key, state))
-    print(cb_tokens)
-
+    print("{} is {}. New tokens:[{}]. New sticky: {}".format(
+        key, state, ", ".join(cb_tokens), sticky))
 
 
 def onSetupSearch(self):
@@ -288,22 +343,53 @@ def onSetupSearch(self):
     for key in default_prefs:
         if key not in prefs:
             prefs[key] = default_prefs[key]
-    self.cbState = prefs
 
     # Create check buttons and hotkeys
     idx = 1
     new_btns = []
+    tokens = []
+    
     for key in order:
         cb = checkboxes[key]
-        if not cb["hotkey"]: # disable checkbutton
+
+        # Set up tokens
+        state = prefs["state"].get(key, None)
+        if not state:
+            state = prefs["state"]["key"] = Qt.Unchecked
+        prefix = cb["prefix"]
+        value = cb["value"]
+        if state:
+            if prefix and value:
+                token = prefix + value
+                tokens.append(token)
+            elif prefix:
+                last = prefs["last"].get(key, None)
+                if last:
+                    tokens += last
+        
+        # Set up buttons and hotkeys
+        hotkey = cb["hotkey"]
+        if not hotkey: # disable checkbutton
             continue
-        b = QCheckBox(cb["label"], widget)
-        b.setToolTip(cb["tooltip"])
-        b.setCheckState(prefs.get(key, Qt.Unchecked))
+        label = cb.get("label", "")
+        tooltip = cb.get("tooltip", "")
+        icons = cb.get("icons", None)
+        
+        b = QCheckBox(label, widget)
+        b.setToolTip(tooltip)
+        b.setFocusPolicy(Qt.NoFocus)
+        
+        if icons:
+            iconset = iconsets[icons]
+            stylesheet = cb_stylesheet.format(*iconset)
+            b.setStyleSheet(stylesheet)
+
+        b.setCheckState(state)
         b.stateChanged.connect(lambda a, k=key:self.onCbStateChanged(a, k))
         new_btns.append(b)
         s = QShortcut(QKeySequence(_(cb["hotkey"])), 
                 self, activated=b.toggle)
+        
         idx += 1
 
     # Add widgets to gridlayout while restructuring it
@@ -318,13 +404,16 @@ def onSetupSearch(self):
     for i, item in enumerate(items):
         layout.addWidget(item, 0, i, 1, 1)
 
+    self.cbPrefs = prefs
+    self.cbTokens = tokens
+
 
 def onCloseEvent(self, evt):
     """Save configuration on Browser exit"""
-    mw.pm.profile["browserCbs"] = self.cbState
+    mw.pm.profile["browserCbs"] = self.cbPrefs
 
 
+Browser.onCbStateChanged = onCbStateChanged
 Browser.setupSearch = wrap(Browser.setupSearch, onSetupSearch, "after")
 Browser.closeEvent = wrap(Browser.closeEvent, onCloseEvent, "before")
-Browser.onCbStateChanged = onCbStateChanged
 Browser.onSearch = wrap(Browser.onSearch, onSearch, "before")
