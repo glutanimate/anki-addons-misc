031eca3e76d2f2e24e0ff88fae1232d35e5bf8b1
Glutanimate <glutanimate@users.noreply.github.com>
2017-08-08 22:20:02 +0200

    New options: tooltip duration, image size, card limits for encouragements

--- src/reviewer_puppy_reinforcement/Puppy Reinforcement.py
+++ src/reviewer_puppy_reinforcement/Puppy Reinforcement.py
@@ -9,10 +9,28 @@ License: GNU AGPLv3 or later <https://www.gnu.org/licenses/agpl.html>
 
 ############## USER CONFIGURATION START ##############
 
-encourage_every = 10 # show encouragement about every n cards; default: 10
-max_spread = 5 # max spread around interval
-tooltip_color = "#AFFFC5" # HTML color code; default: light green
-encouragements = {
+# Appearance
+
+# duration in msec; default: 3000 (=3 sec.)
+DURATION = 3000
+# image height in px, tooltip is automatically scaled; default: 128
+IMAGE_HEIGHT = 128
+# HTML color code; default: #AFFFC5 (light green)
+TOOLTIP_COLOR = "#AFFFC5"
+
+
+# Behavior
+
+# show encouragement about every n cards; default: 10
+ENCOURAGE_EVERY = 10
+# max spread around interval; default: 5
+MAX_SPREAD = 5
+# lowe card limits for encouragement levels (defaults: 100, 50, 25)
+LIMIT_MAX = 100
+LIMIT_HIGH = 50
+LIMIT_MIDDLE = 25
+# encouragements by level
+ENCOURAGEMENTS = {
     "low": ["Great job!", "Keep it up!", "Way to go!", "Keep up the good work!"],
     "middle": ["You're on a streak!", "You're crushing it!", "Don't stop now!",
             "You're doing great!"],
@@ -33,7 +51,7 @@ mw.dogs = {
     "cnt": 0,
     "last": 0,
     "enc": None,
-    "ivl": encourage_every
+    "ivl": ENCOURAGE_EVERY
 }
 
 dogs_dir = os.path.join(mw.pm.addonFolder(), 'puppy_reinforcement')
@@ -42,7 +60,7 @@ dogs_imgs = [i for i in os.listdir(dogs_dir) if i.endswith(".jpg")]
 _tooltipTimer = None
 _tooltipLabel = None
 
-def dogTooltip(msg, image=":/icons/help-hint.png", period=3000, parent=None):
+def dogTooltip(msg, image=":/icons/help-hint.png", period=DURATION, parent=None):
     global _tooltipTimer, _tooltipLabel
     class CustomLabel(QLabel):
         def mousePressEvent(self, evt):
@@ -53,21 +71,22 @@ def dogTooltip(msg, image=":/icons/help-hint.png", period=3000, parent=None):
     lab = CustomLabel("""\
 <table cellpadding=10>
 <tr>
-<td><img height=128 src="%s"></td>
+<td><img height=%d src="%s"></td>
 <td valign="middle">
     <center><b>%i cards done so far!</b><br>%s</center>
 </td>
 </tr>
-</table>""" % (image, mw.dogs["cnt"], msg), aw)
+</table>""" % (IMAGE_HEIGHT, image, mw.dogs["cnt"], msg), aw)
     lab.setFrameStyle(QFrame.Panel)
     lab.setLineWidth(2)
     lab.setWindowFlags(Qt.ToolTip)
     p = QPalette()
-    p.setColor(QPalette.Window, QColor(tooltip_color))
+    p.setColor(QPalette.Window, QColor(TOOLTIP_COLOR))
     p.setColor(QPalette.WindowText, QColor("#000000"))
     lab.setPalette(p)
+    vdiff = IMAGE_HEIGHT - 128
     lab.move(
-        aw.mapToGlobal(QPoint(0, -260 + aw.height())))
+        aw.mapToGlobal(QPoint(0, -260-vdiff + aw.height())))
     lab.show()
     _tooltipTimer = mw.progress.timer(
         period, closeTooltip, False)
@@ -88,14 +107,14 @@ def closeTooltip():
 
 def getEncouragement(cards):
     last = mw.dogs["enc"]
-    if cards >= 100:
-        lst = list(encouragements["max"])
-    elif cards >= 50:
-        lst = list(encouragements["high"])
-    elif cards >= 25:
-        lst = list(encouragements["middle"])
+    if cards >= LIMIT_MAX:
+        lst = list(ENCOURAGEMENTS["max"])
+    elif cards >= LIMIT_HIGH:
+        lst = list(ENCOURAGEMENTS["high"])
+    elif cards >= LIMIT_MIDDLE:
+        lst = list(ENCOURAGEMENTS["middle"])
     else:
-        lst = list(encouragements["low"])
+        lst = list(ENCOURAGEMENTS["low"])
     if last and last in lst:
         # skip identical encouragement
         lst.remove(last)
@@ -111,7 +130,7 @@ def showDog():
     msg = getEncouragement(mw.dogs["cnt"])
     dogTooltip(msg, image=image_path)
     # intermittent reinforcement:
-    mw.dogs["ivl"] = max(1, encourage_every + random.randint(-max_spread,max_spread))
+    mw.dogs["ivl"] = max(1, ENCOURAGE_EVERY + random.randint(-MAX_SPREAD,MAX_SPREAD))
     mw.dogs["last"] = mw.dogs["cnt"]
 
 mw.reviewer.nextCard = wrap(mw.reviewer.nextCard, showDog)
