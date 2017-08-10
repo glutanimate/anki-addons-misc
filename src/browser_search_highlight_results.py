1b2991bc0ef0c840588017818d6c9231aca565c4
Glutanimate <glutanimate@users.noreply.github.com>
2017-08-10 22:36:08 +0200

    Ability to toggle result highlighting on/off

--- src/browser_search_highlight_results.py
+++ src/browser_search_highlight_results.py
@@ -3,11 +3,14 @@
 """
 Anki Add-on: Highlight Search Results in Browser
 
-Highlights search results in editor pane of the Browser and adds two
+Highlights search results in Editor pane of the Browser and adds two
 new shortcuts when searching:
 
 - Shift-Return: Jump to result in current list 
 - Ctrl-Shift-Return: Select results in current list
+
+Search result highlighting may be turned off via the provided View
+menu entry, or its associated hotkey (Ctrl+T, H).
 
 Limitations: Searches through entire editor screen,
              field descriptions included
@@ -18,10 +21,18 @@ License: GNU AGPLv3 or later <https://www.gnu.org/licenses/agpl.html>
 
 from __future__ import unicode_literals
 
+############## USER CONFIGURATION START ##############
+
+HIGHLIGHT_BY_DEFAULT = True
+HOTKEY_HIGHLIGHT_TOGGLE = "Ctrl+T, H"
+
+##############  USER CONFIGURATION END  ##############
+
 from aqt.qt import *
 from aqt.browser import Browser
-from anki.hooks import wrap
+from anki.hooks import wrap, addHook
 
+# ignore search token specifiers, search operators, and wildcard characters
 excluded_tags = ("deck:", "tag:", "card:", "note:", "is:", "prop:", "added:",
                 "rated:", "nid:", "cid:", "mid:", "seen:")
 excluded_vals = ("*", "_", "_*")
@@ -32,6 +43,8 @@ def onRowChanged(self, current, previous):
     """
     Highlight search results in Editor pane on searching
     """
+    if not self._highlightResults:
+        return
     txt = self.form.searchEdit.lineEdit().text().strip()
     if not txt:
         return
@@ -83,6 +96,16 @@ def onCustomSearch(self, onecard=False):
     self.model.restoreSelection()
 
 
+def toggleSearchHighlights(self, checked):
+    """Toggle search highlights on or off"""
+    self._highlightResults = checked
+    if not checked:
+        # clear highlights
+        self.editor.web.findText("", QWebPage.HighlightAllOccurrences)
+    else:
+        onRowChanged(self, None, None)
+
+
 def onSetupSearch(self):
     """Add extended search hotkeys"""
     s = QShortcut(QKeySequence(_("Shift+Return")), self.form.searchEdit)
@@ -91,6 +114,28 @@ def onSetupSearch(self):
     s.activated.connect(self.onCustomSearch)
 
 
+def onSetupMenus(self):
+    """Setup menu entries and hotkeys"""
+    self._highlightResults = HIGHLIGHT_BY_DEFAULT
+    try:
+        # used by multiple add-ons, so we check for its existence first
+        menu = self.menuView
+    except:
+        self.menuView = QMenu(_("&View"))
+        action = self.menuBar().insertMenu(
+            self.mw.form.menuTools.menuAction(), self.menuView)
+        menu = self.menuView
+    menu.addSeparator()
+    a = menu.addAction('Highlight Search Results')
+    a.setCheckable(True)
+    a.setChecked(HIGHLIGHT_BY_DEFAULT)
+    a.setShortcut(QKeySequence(HOTKEY_HIGHLIGHT_TOGGLE))
+    a.toggled.connect(self.toggleSearchHighlights)
+
+
+
+addHook("browser.setupMenus", onSetupMenus)
 Browser.onCustomSearch = onCustomSearch
+Browser.toggleSearchHighlights = toggleSearchHighlights
 Browser.onRowChanged = wrap(Browser.onRowChanged, onRowChanged, "after")
 Browser.setupSearch = wrap(Browser.setupSearch, onSetupSearch, "after")
