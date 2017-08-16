af7dc5c4d24a944139cc15d244eea5786a37b7d8
Glutanimate <glutanimate@users.noreply.github.com>
2017-08-16 05:52:57 +0200

    Add separator before menu section; Add option for customizable stylesheet

--- src/common_context_search.py
+++ src/common_context_search.py
@@ -27,7 +27,23 @@ SEARCH_PROVIDERS = [
     ("Wikipedia (&de)", u"https://de.wikipedia.org/w/index.php?search=%s")
 ]
 
+# (Advanced) Use custom context menu style sheet, somewhat buggy
+USE_CUSTOM_STYLESHEET = False 
+
 ##############  USER CONFIGURATION END  ##############
+
+stylesheet = """
+QMenu::item {
+    padding-top: 15px;
+    padding-bottom: 15px;
+    padding-right: 10px;
+    padding-left: 10px;
+}
+QMenu::item:selected {
+    color: black;
+    background-color: #D9CD6D;
+}
+"""
 
 import urllib
 
@@ -48,12 +64,15 @@ def lookup_online(text, idx):
 
 def add_lookup_action(view, menu):
     """Add 'lookup' action to context menu"""
+    if USE_CUSTOM_STYLESHEET:
+        menu.setStyleSheet(stylesheet)
     selected = view.page().selectedText()
     if not selected:
         return
     
     suffix = (selected[:20] + '..') if len(selected) > 20 else selected
     label = u'Search for "%s" in Card &Browser' % suffix
+    menu.addSeparator()
     a = menu.addAction(label)
     a.triggered.connect(lambda _, t=selected: lookup_browser(t))
 
