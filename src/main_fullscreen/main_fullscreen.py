e4f6b885b91785b740a78a78151c9fa7b99509ea
Glutanimate <glutanimate@users.noreply.github.com>
2017-09-06 20:08:23 +0200

    Implement custom undo shortcut that's only active when maximized
    Anki's regular undo hotkey is disabled due to hiding the menu.

--- src/main_fullscreen/main_fullscreen.py
+++ src/main_fullscreen/main_fullscreen.py
@@ -24,15 +24,32 @@ KEY_FULLSCREEN_TOGGLE = "F11"
 
 from aqt.qt import *
 from aqt import mw
+from anki.lang import _
+
 
 def onFullScreen():
     if not mw.isFullScreen():
         mw.setWindowState(mw.windowState() | Qt.WindowFullScreen)
         mw.menuBar().hide()
+        custom_undo.setEnabled(True)
     else:
         mw.setWindowState(mw.windowState() ^ Qt.WindowFullScreen)
         mw.menuBar().show()
-        
+        custom_undo.setEnabled(False)
+    
+def myUndo():
+    try:
+        mw.onUndo()
+    except TypeError: # no more steps back
+        pass
+
+
+# Restore undo shortcut that would otherwise be disabled
+# due to removing the menu
+custom_undo = QShortcut(QKeySequence(_("Ctrl+Z")), mw,
+    activated=myUndo)
+custom_undo.setEnabled(False)
+
 
 QShortcut(QKeySequence(KEY_FULLSCREEN_TOGGLE), mw, 
     activated=onFullScreen)
