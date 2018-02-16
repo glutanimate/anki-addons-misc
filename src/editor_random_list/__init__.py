c58c6415c5ba5520875fe889adbf194822504390
Glutanimate <glutanimate@users.noreply.github.com>
2018-02-17 00:23:06 +0100

    Restructured editor_random_list for new build tools

---
+++ src/editor_random_list/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Entry point for the add-on into Anki
+# Please do not edit this if you do not know what you are doing.
+#
+# Copyright: (c) 2017-2018 Glutanimate <https://glutanimate.com/>
+# License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
+
+from . import editor_random_list
