d10e5e300518b14aa03d65ee5ff930a82cb9853c
liuzikai <liuzikai@163.com>
2018-08-22 11:47:36 +0800

    Update ProgressBar: Use a robuster method to get due card counts. Introduce weights into calculation to make it go more normally. Re-organize code.

--- src/reviewer_progress_bar/reviewer_progress_bar.py
+++ src/reviewer_progress_bar/reviewer_progress_bar.py
@@ -16,10 +16,12 @@ from __future__ import unicode_literals
 from anki.hooks import addHook, wrap
 from anki import version as anki_version
 
+from aqt.utils import showInfo
+
 from aqt.qt import *
 from aqt import mw
 
-__version__ = '1.3.0'
+__version__ = '2.0.0'
 
 ############## USER CONFIGURATION START ##############
 
@@ -33,34 +35,64 @@ includeLrn = True
 # Only include new cards once reviews are exhausted.
 includeNewAfterRevs = True
 
-# Limit count to your review settings as opposed to deck overall
-limitToReviewSettings = True
+# Calculation weights
+#
+#   Setting proper weights will make the progress bar goes smoothly and decrease the chance of going backwards.
+#
+#   For example, if all weight are 1, and you set 2 steps for a new card in your desk config, you will convey
+#   one 'new' into two 'learning' card if you press 'again' at the first time, which will increase remaining
+#   count and cause the bar to move backward.
+#
+#   In this case, it's not a bad idea to set newWeight to 2, and remaining count will be calculated as
+#   new * 2 + learn + review.Now pressing 'again' will just make it stop going forward, but not backward. If
+#   you press 'esay' at first, the progress will go twice as fast, which is still reasonable.
+#
+#   However, if you press 'good' followed by 'again', there will be another two learning card again, and the
+#   progress still needs to go backward. It may not be a big deal, but if you want the progress never goes
+#   backward strictly, enable forceForward below.
+#
+#   Weights should be integers. It's their relative sizes that matters, not absolute values.
+#
+#   Another example that make the progress goes unstably is 'bury related new cards to next day.' If you have
+#   three new cards in a note, there will be 3 new cards at the beginning of your review, but another two will
+#   disappear instantly after you learn one of them. However, all three cards will be regarded as 'completed,'
+#   so your progress may go three times as fast.
+#
+newWeight = 2
+revWeight = 1
+lrnWeight = 1
+
+# If enabled, the progress will freeze if remaining count has to increase to prevent moving backward,
+#   and wait until your correct answers 'make up' this additional part.
+#   NOTE: This will not stop the progress from moving backward if you add cards or toggle suspended.
+forceForward = False
 
 # PROGRESS BAR APPEARANCE
 
-showPercent = False # Show the progress text percentage or not.
-showNumber = False # Show the progress text as a fraction
+showPercent = False  # Show the progress text percentage or not.
+showNumber = False  # Show the progress text as a fraction
 
-
-qtxt = "aliceblue" # Percentage color, if text visible.
-qbg = "#18adab" # Background color of progress bar.
-qfg = "#38e3b2" # Foreground color of progress bar.
-qbr = 0 # Border radius (> 0 for rounded corners).
+qtxt = "aliceblue"  # Percentage color, if text visible.
+qbg = "#ececec"  # Background color of progress bar.
+qfg = "#3399cc"  # Foreground color of progress bar.
+qbr = 0  # Border radius (> 0 for rounded corners).
 
 # optionally restricts progress bar width
-maxWidth = ""  # (e.g. "5px". default: "")
+maxWidth = "5px"  # (e.g. "5px". default: "")
 
-orientationHV = Qt.Horizontal # Show bar horizontally (side to side). Use with top/bottom dockArea.
+floatingBarWhenEditing = True # Make the progress bar 'floating' when waiting to resume.
+
+orientationHV = Qt.Horizontal  # Show bar horizontally (side to side). Use with top/bottom dockArea.
 # orientationHV = Qt.Vertical # Show bar vertically (up and down). Use with right/left dockArea.
 
-invertTF = False # If set to True, inverts and goes from right to left or top to bottom.
+invertTF = False  # If set to True, inverts and goes from right to left or top to bottom.
 
-dockArea = Qt.TopDockWidgetArea # Shows bar at the top. Use with horizontal orientation.
-#dockArea = Qt.BottomDockWidgetArea # Shows bar at the bottom. Use with horizontal orientation.
-#dockArea = Qt.RightDockWidgetArea # Shows bar at right. Use with vertical orientation.
-#dockArea = Qt.LeftDockWidgetArea # Shows bar at left. Use with vertical orientation.
+dockArea = Qt.TopDockWidgetArea  # Shows bar at the top. Use with horizontal orientation.
+# dockArea = Qt.BottomDockWidgetArea # Shows bar at the bottom. Use with horizontal orientation.
+# dockArea = Qt.RightDockWidgetArea # Shows bar at right. Use with vertical orientation.
+# dockArea = Qt.LeftDockWidgetArea # Shows bar at left. Use with vertical orientation.
 
-pbStyle = "" # Stylesheet used only if blank. Else uses QPalette + theme style.
+pbStyle = ""  # Stylesheet used only if blank. Else uses QPalette + theme style.
 '''pbStyle options (insert a quoted word above):
     -- "plastique", "windowsxp", "windows", "windowsvista", "motif", "cde", "cleanlooks"
     -- "macintosh", "gtk", or "fusion" might also work
@@ -72,15 +104,22 @@ pbStyle = "" # Stylesheet used only if blank. Else uses QPalette + theme style.
 
 ## Set up variables
 
-nm = 0
-failed = 0
+remainCount = {}  # {did: remaining count (weighted) of the deck}, calculated with data from Anki col and sched
+doneCount = {}  # {did: done count (weighted) of the deck}, calculated as total - remain when showing next question
+totalCount = {}  # {did: max total count (weighted) that was seen}, calculated as remain + done after state change
+# NOTE: did stands for 'deck id'
+# See comments on updateCounts() for further usage.
+
+
+currDID = None  # current deck id (None means at the deck browser)
+
+nmStyleApplied = 0
+nmUnavailable = 0
 progressBar = None
-mx = 0
-limitedBarLength = 0
 
-pbdStyle = QStyleFactory.create("%s" % (pbStyle)) # Don't touch.
+pbdStyle = QStyleFactory.create("%s" % (pbStyle))  # Don't touch.
 
-#Defining palette in case needed for custom colors with themes.
+# Defining palette in case needed for custom colors with themes.
 palette = QPalette()
 palette.setColor(QPalette.Base, QColor(qbg))
 palette.setColor(QPalette.Highlight, QColor(qfg))
@@ -99,9 +138,10 @@ else:
 try:
     # Remove that annoying separator strip if we have Night Mode, avoiding conflicts with this add-on.
     import Night_Mode
+
     Night_Mode.nm_css_menu \
-    += Night_Mode.nm_css_menu \
-    + '''
+        += Night_Mode.nm_css_menu \
+           + '''
         QMainWindow::separator
     {
         width: 0px;
@@ -109,24 +149,38 @@ try:
     }
     '''
 except ImportError:
-    failed = 1
+    nmUnavailable = 1
 
 
-def nmc():
-    """Checks whether Night_Mode is disabled:
-        if so, we remove the separator here."""
-    global nm
-    if not failed:
-        nm = Night_Mode.nm_state_on
-    if not nm:
-        mw.setStyleSheet(
-    '''
-        QMainWindow::separator
-    {
-        width: 0px;
-        height: 0px;
-    }
-    ''')
+def initPB():
+    """Initialize and set parameters for progress bar, adding it to the dock."""
+    global progressBar
+    progressBar = QProgressBar()
+    progressBar.setTextVisible(showPercent or showNumber)
+    progressBar.setInvertedAppearance(invertTF)
+    progressBar.setOrientation(orientationHV)
+    if pbdStyle == None:
+        progressBar.setStyleSheet(
+            '''
+                    QProgressBar
+                {
+                    text-align:center;
+                    color:%s;
+                    background-color: %s;
+                    border-radius: %dpx;
+                    %s
+                }
+                    QProgressBar::chunk
+                {
+                    background-color: %s;
+                    margin: 0px;
+                    border-radius: %dpx;
+                }
+                ''' % (qtxt, qbg, qbr, restrictSize, qfg, qbr))
+    else:
+        progressBar.setStyle(pbdStyle)
+        progressBar.setPalette(palette)
+    _dock(progressBar)
 
 
 def _dock(pb):
@@ -136,8 +190,8 @@ def _dock(pb):
     tWidget = QWidget()
     dock.setObjectName("pbDock")
     dock.setWidget(pb)
-    dock.setTitleBarWidget( tWidget )
-    
+    dock.setTitleBarWidget(tWidget)
+
     ## Note: if there is another widget already in this dock position, we have to add ourself to the list
 
     # first check existing widgets
@@ -164,146 +218,184 @@ def _dock(pb):
     return dock
 
 
-def pb():
-    """Initialize and set parameters for progress bar, adding it to the dock."""
-    mx = max(1, getMX())
-    progressBar = QProgressBar()
-    progressBar.setRange(0, mx)
-    progressBar.setTextVisible(showPercent or showNumber)
-    progressBar.setInvertedAppearance(invertTF)
-    progressBar.setOrientation(orientationHV)
-    if pbdStyle == None:
-        progressBar.setStyleSheet(
-        '''
-                    QProgressBar
-                {
-                    text-align:center;
-                    color:%s;
-                    background-color: %s;
-                    border-radius: %dpx;
-                    %s
-                }
-                    QProgressBar::chunk
-                {
-                    background-color: %s;
-                    margin: 0px;
-                    border-radius: %dpx;
-                }
-                ''' % (qtxt, qbg, qbr, restrictSize, qfg, qbr))
+def updatePB():
+    """Update progress bar range and value with currDID, totalCount[] and doneCount[]"""
+    if currDID:
+        # In a specific deck
+        deckName = mw.col.decks.name(currDID)
+        pbMax = pbValue = 0
+        # Sum up all children decks
+        for deckProp in mw.col.sched.deckDueList():
+            name = deckProp[0]
+            did = deckProp[1]
+            if name.startswith(deckName):
+                pbMax += totalCount[did]
+                pbValue += doneCount[did]
     else:
-        progressBar.setStyle(pbdStyle)
-        progressBar.setPalette(palette)
-    _dock(progressBar)
-    return progressBar, mx
+        # At desk browser
+        pbMax = sum(totalCount.values())
+        pbValue = sum(doneCount.values())
+    # showInfo("pbMax = %d, pbValue = %d" % (pbMax, pbValue))
 
+    if pbMax == 0:
+        progressBar.setRange(0, 1)
+        progressBar.setValue(1)
+    else:
+        progressBar.setRange(0, pbMax)
+        progressBar.setValue(pbValue)
 
-def getMX():
-    """Get deck's card counts for progress bar updates."""
-    rev = nu = lrn = 0
-    if includeRev:
-        rev = mw.col.sched.totalRevForCurrentDeck()
-    if includeLrn:
-        try:
-            lrn = mw.col.sched.lrnCount
-        except AttributeError:
-            pass
-    if includeNew or (includeNewAfterRevs and rev == 0):
-        nu = mw.col.sched.totalNewForCurrentDeck()
-    total = rev + nu + lrn
-    return total
-
-def _getLimitedBarLength():
-    """ Get a new bar length based off the number of new / lrn / rev cards you have left for the day """
-    global limitedBarLength
-    active_decks = mw.col.decks.active()
-    if len(active_decks) > 0:
-        rev = lrn = nu = 0
-
-        # get number of cards
-        for tree in [deck for deck in mw.col.sched.deckDueList() if deck[1] in active_decks]:
-            if includeRev:
-                rev += tree[2]
-            if includeLrn:
-                lrn += tree[3]
-            if includeNew or (includeNewAfterRevs and rev == 0):
-                nu += tree[4]
-            
-        if nu + rev < mx:
-            limitedBarLength = nu+lrn+rev
-            return
-    limitedBarLength = -1
-
-def _updatePB():
-    """Update progress bar; hiding/showing prevents flashing bug."""
-    global mx
-    if progressBar:
-        nmc()
-        total = getMX()
-        if total > mx:
-            mx = total
-            progressBar.setRange(0, mx)
-        curr = (mx - total)
-        progressBar.hide()
-
-        barSize = mx
-        if limitToReviewSettings and limitedBarLength != -1:
-            barSize = limitedBarLength
-
-            # I this can happen if the number of "rev" cards increases during the study session
-            if curr > barSize:
-                barSize = curr
-            progressBar.setRange(0, barSize)
-            
-        if showNumber:
-            if showPercent:
-                percent = 100 if barSize==0 else int(100*curr/barSize)
-                progressBar.setFormat("%d / %d (%d%%)" % (curr, barSize, percent))
-            else:
-                progressBar.setFormat("%d / %d" % (curr, barSize))
-        progressBar.setValue(curr)
-        progressBar.show()
-
-
-
-def _renderBar(state, oldState):
-    global mx, progressBar
-    if state == "overview":
-        # Set up progress bar at deck's overview page: initialize or modify.
-        if not progressBar: progressBar, mx = pb()
-        else: rrenderPB()
-        _getLimitedBarLength()
-        if showNumber:
-            _updatePB()
-        progressBar.show()
-        nmc()
-    elif state == "deckBrowser":
-        # Hide the progress bar at deck list. Not deleted, so we just modify later.
-        if progressBar:
-            mx = 0
-            progressBar.hide()
-
-
-def rrenderPB():
-    """Modify progress bar if it was already initialized."""
-    global mx
-    if getMX() >= 1:
-        if mx > getMX(): _updatePB()
+    if showNumber:
+        if showPercent:
+            percent = 100 if pbMax == 0 else int(100 * pbValue / pbMax)
+            progressBar.setFormat("%d / %d (%d%%)" % (pbValue, pbMax, percent))
         else:
-            mx = getMX()
-            progressBar.setRange(0, mx)
-            progressBar.reset()
-    else: progressBar.setValue(mx)
+            progressBar.setFormat("%d / %d" % (pbValue, pbMax))
+    nmApplyStyle()
 
 
-addHook("afterStateChange", _renderBar)
-addHook("showQuestion", _updatePB)
+def setFloatingPB():
+    """Make progress bar in waiting style if the state is resetRequired (happened after editing cards.)"""
+    progressBar.setRange(0, 0)
+    if showNumber:
+        progressBar.setFormat("Waiting...")
+    nmApplyStyle()
 
+
+def nmApplyStyle():
+    """Checks whether Night_Mode is disabled:
+        if so, we remove the separator here."""
+    global nmStyleApplied
+    if not nmUnavailable:
+        nmStyleApplied = Night_Mode.nm_state_on
+    if not nmStyleApplied:
+        mw.setStyleSheet(
+            '''
+        QMainWindow::separator
+    {
+        width: 0px;
+        height: 0px;
+    }
+    ''')
+
+
+def calcCount(revRaw, lrnRaw, newRaw):
+    """Calculate count with weights with raw values from the sched."""
+    rev = lrn = new = 0
+    if includeRev:
+        rev = revRaw * revWeight
+    if includeLrn:
+        lrn = lrnRaw * lrnWeight
+    if includeNew or (includeNewAfterRevs and rev == 0):
+        new = newRaw * newWeight
+    return rev + lrn + new
+
+
+def initCounts():
+    """Iterate decks and initialize totalCount[], remainCount[] and doneCount[]."""
+    for deckProp in mw.col.sched.deckDueList():
+        _initCounts(deckProp[1], calcCount(deckProp[2], deckProp[3], deckProp[4]))
+
+
+def _initCounts(did, remain):
+    totalCount[did] = remainCount[did] = remain
+    doneCount[did] = 0
+
+
+# When state change (happens after adding, editing or deleting cards), forceUpdateTotal = True,
+#   totalCount = doneCount + remainCount. User doesn't answer questions during this process, so doneCount should not
+#   change. Changes in remainCount are caused by user's adding, editing or deleting, so totalCount should be
+#   re-evaluated based on doneCount + remainCount.
+# When 'showQuestion' is triggered, forceUpdateTotal = False, doneCount = totalCount - remainCount. The changes in
+#    remainCount are expected to be caused by answering questions, so the changed parts should be regarded as 'done.'
+
+
+def updateCounts(forceUpdateTotal):
+    """Update totalCount[], remainCount[] and doneCount[].
+       If forceUpdateTotal, update totalCount based on doneCount and remainCount.
+       If not, update doneCount based on remainCount and totalCount, and increase totalCount only if needed."""
+    # if currDID:
+    #     # When in reviewer, to get correct card count, we have to use the following codes.
+    #     # Excerpted from Anki reviewer
+    #     if mw.reviewer.hadCardQueue:
+    #         counts = list(mw.col.sched.counts())
+    #     else:
+    #         counts = list(mw.col.sched.counts(mw.reviewer.card))
+    #
+    #     _updateCounts(currDID, calcCount(counts[2], counts[1], counts[0]), forceUpdateTotal)
+    # else:
+    availableDID = []  # record exist deck ids, for cleaning up
+    for deckProp in mw.col.sched.deckDueList():
+        did = deckProp[1]
+        availableDID.append(did)
+        remain = calcCount(deckProp[2], deckProp[3], deckProp[4])
+        if did not in totalCount.keys():
+            _initCounts(did, remain)
+            # showInfo("New inited deck %s, remain = %d" % (deckProp[0], remain))
+        else:
+            _updateCounts(did, remain, forceUpdateTotal)
+
+    # delete counts of unavailable decks
+    for did in totalCount.keys():
+        if did not in availableDID:
+            del totalCount[did], remainCount[did], doneCount[did]
+
+
+def _updateCounts(did, remain, forceUpdateTotal):
+    remainCount[did] = remain
+
+    if forceUpdateTotal:
+        totalCount[did] = doneCount[did] + remainCount[did]  # See comments above updateCounts() for explanation.
+    else:
+        if remainCount[did] + doneCount[did] > totalCount[did]:
+            if forceForward:
+                return  # Give up changing counts, until the remainCount decrease.
+            else:
+                # This may happen if you press 'again' followed by 'good' for a new card, as stated in comments
+                #    'Calculation weights,' or when you undo a card, making remaining count increases.
+
+                # showInfo("Not forced update total from %d to %d" % (totalCount[did], doneCount[did] + remainCount[did]))
+                totalCount[did] = doneCount[did] + remainCount[did]
+        # showInfo("Confirm doneCount changes, did %d" % did)
+        doneCount[did] = totalCount[did] - remainCount[did]  # See comments above updateCounts() for explanation.
+
+
+def afterStateChangeCallBack(state, oldState):
+    global currDID
+
+    if state == "resetRequired":
+        if floatingBarWhenEditing:
+            setFloatingPB()
+        return
+    elif state == "deckBrowser":
+        # initPB() has to be here, since objects are not prepared yet when the add-on is loaded.
+        if not progressBar:
+            initPB()
+            initCounts()
+        currDID = None
+    else:  # "overview" or "review"
+        # showInfo("mw.col.decks.current()['id'])= %d" % mw.col.decks.current()['id'])
+        currDID = mw.col.decks.current()['id']
+
+    # showInfo("updateCounts(False) 0, currDID = %d" % (currDID if currDID else 0))
+    updateCounts(True)  # See comments above updateCounts() for explanation.
+    updatePB()
+
+
+def showQuestionCallBack():
+    # showInfo("updateCounts(False) 1, currDID = %d" % (currDID if currDID else 0))
+    updateCounts(False)  # See comments above updateCounts() for explanation.
+    updatePB()
+
+
+addHook("afterStateChange", afterStateChangeCallBack)
+addHook("showQuestion", showQuestionCallBack)
 
 if anki_version.startswith("2.0.x"):
     """Workaround for QSS issue in EditCurrent,
     only necessary on Anki 2.0.x"""
 
     from aqt.editcurrent import EditCurrent
+
 
     def changeStylesheet(*args):
         mw.setStyleSheet('''
@@ -314,8 +406,10 @@ if anki_version.startswith("2.0.x"):
         }
         ''')
 
+
     def restoreStylesheet(*args):
         mw.setStyleSheet("")
+
 
     EditCurrent.__init__ = wrap(
         EditCurrent.__init__, restoreStylesheet, "after")
