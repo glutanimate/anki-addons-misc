8a09accff68dc4648b835a1fb78a5692b8f4db1e
ijgnd <fooijgnd@barijgnd>
2019-04-16 01:15:50 +0200

    common_ctrlf_search - reorganize files

---
+++ src/common_ctrlf_search/common ctrlf search.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+Anki Add-on: Ctrl+F Search
+Entry point for the add-on into Anki
+
+Copyright: (c) 2017-2019 Glutanimate <https://glutanimate.com/>
+
+License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
+"""
+
+# IMPORTANT:
+# 
+# If you are reading this and wondering where to find the
+# configuration section for Anki 2.0:
+#
+# As part of porting this add-on to Anki 2.1 I had to move the 
+# main add-on module to a separate folder. It is now located under
+# common_ctrlf_search/common_ctrlf_search.py. 
+# All configuration options are located
+# at the very top of that file.
+#
+# Please use NotePad++ or another text editor that supports python
+# source code files to edit it
+
+import common_ctrlf_search # noqa: F401
